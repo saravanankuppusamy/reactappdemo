803dfe93549632430a03faabad5a5e1c70cb117f
Saravanan Kuppusamy <saravanan_kuppusamy@yahoo.com>
2022-06-17 15:42:22 -0400

    modified App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Jenkins
+          Learn React with Jenkins Pipeline
         </a>
       </header>
     </div>
